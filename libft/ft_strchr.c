8560ba9e6cc3ba3f4ee11d7d307b6dbbcf7005f8
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-04-24 20:16:36 -0300

    added libft

---
+++ libft/ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byoshimo <byoshimo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/18 00:35:55 by byoshimo          #+#    #+#             */
+/*   Updated: 2022/09/18 00:35:57 by byoshimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if ((unsigned char)c == '\0')
+		return (&((char *)s)[ft_strlen(s)]);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (unsigned char)c)
+			return (&((char *)s)[i]);
+		i++;
+	}
+	return (NULL);
+}
