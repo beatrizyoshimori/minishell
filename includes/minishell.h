9c3024a35d3e852c37cd56d986d29f9930a345a8
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-04-24 20:17:22 -0300

    tests

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,28 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 19:06:19 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/04/24 19:06:54 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/04/24 20:16:05 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+# include "libft.h"
+# include <stdio.h>
+# include <readline/readline.h>
+// # include <readline/history.h>
 
+typedef struct s_ms
+{
+	
+}	t_ms;
+
+typedef struct s_cmd
+{
+	char			*cmd;
+	char			type;
+	struct s_cmd	*next;
+}	t_cmd;
 
 #endif
--- sources/minishell.c
+++ sources/minishell.c
@@ -6,13 +6,31 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:48:53 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/04/24 19:09:03 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/04/24 20:06:14 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	create_prompt(t_cmd **cmd)
+{
+	t_cmd	*tmp;
+	char	*prompt;
+	char	**line;
+
+	tmp = *cmd;
+	while (1)
+	{
+		prompt = readline("lúbia> ");
+		printf("%s\n", prompt);
+		line = ft_split(prompt, ' ');
+	}
+}
+
 int main(void)
 {
+	t_cmd	*cmd;
+
+	create_prompt(&cmd);
 	return (0);
 }
