4428a04bc03d87771e3423bf382fd45c73b20a70
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-04-25 22:03:54 -0300

    space checking

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:48:53 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/04/24 21:53:02 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/04/25 19:39:09 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,6 @@ int	count_commands(char **line)
 	return (cmds);
 }
 
-t_cmd	*ft_cmd_last(t_cmd *cmd)
-{
-	if (cmd == NULL)
-		return (cmd);
-	while (cmd->next != NULL)
-		cmd = cmd->next;
-	return (cmd);
-}
-
-void	ft_cmdadd_back(t_cmd **cmd, t_cmd *new)
-{
-	if (*cmd == NULL)
-	{
-		*cmd = new;
-		return ;
-	}
-	ft_cmd_last(*cmd)->next = new;
-}
-
-
 void	read_commands(char **line, t_cmd **cmd, t_ms *ms)
 {
 	t_cmd	*new;
@@ -58,7 +38,111 @@ void	read_commands(char **line, t_cmd **cmd, t_ms *ms)
 	{
 		new = (t_cmd *)malloc(sizeof(t_cmd));
 		new->cmd = line[i];
-		
+		i++;
+	}
+}
+
+int	check_no_spaces(char *prompt, char c)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (prompt[i])
+	{
+		if (prompt[i] == c)
+		{
+
+			if (!((prompt[i - 1] >= 9 && prompt[i - 1] <= 13)
+				|| prompt[i - 1] == 32))
+				count++;
+			if ((prompt[i] == '<' && prompt[i + 1] == '<')
+				|| prompt[i] == '>' && prompt[i + 1] == '>')
+				i++;
+			if (!((prompt[i + 1] >= 9 && prompt[i + 1] <= 13)
+				|| prompt[i + 1] == 32))
+				count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+void	copy_prompt(char *prompt, char *new_prompt)
+{
+	int	i;
+
+	i = 0;
+	while (prompt[i])
+	{
+		i++;
+	}
+}
+
+void	create_spaces(char *prompt)
+{
+	int		length;
+	char	*new_prompt;
+
+	length = ft_strlen(prompt);
+	length += check_no_spaces(prompt, '|');
+	length += check_no_spaces(prompt, '<');
+	length += check_no_spaces(prompt, '>');
+	if (length != ft_strlen(prompt))
+	{
+		new_prompt = (char *)ft_calloc(length + 1, sizeof(char));
+		copy_prompt(prompt, new_prompt);
+	}
+	return ;
+}
+
+void	redirections_spaces(char *prompt)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (prompt[i])
+	{
+		if (prompt[i] == '>' || prompt[i] == '<')
+		{
+			j = i - 1;
+			while ((prompt[j] >= 9 && prompt[j] <= 13) || prompt[j] == 32)
+				prompt[j--] = -2;
+			i++;
+			if ((prompt[i - 1] == '>' && prompt[i] == '>')
+				|| (prompt[i - 1] == '<' && prompt[i] == '<'))
+				i++;
+			while ((prompt[i] >= 9 && prompt[i] <= 13) || prompt[i] == 32)
+				i++;
+			while (!((prompt[i] >= 9 && prompt[i] <= 13) || prompt[i] == 32))
+				i++;
+			while ((prompt[i] >= 9 && prompt[i] <= 13) || prompt[i] == 32)
+				prompt[i++] = -2;
+		}
+		i++;
+	}
+}
+
+void	pipe_spaces(char *prompt)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (prompt[i])
+	{
+		if (prompt[i] == '|')
+		{
+			j = i - 1;
+			while ((prompt[j] >= 9 && prompt[j] <= 13) || prompt[j] == 32)
+				prompt[j--] = -1;
+			j = i + 1;
+			while ((prompt[j] >= 9 && prompt[j] <= 13) || prompt[j] == 32)
+				prompt[j++] = -1;
+		}
+		i++;
 	}
 }
 
@@ -70,10 +154,12 @@ void	create_prompt(t_cmd **cmd, t_ms **ms)
 	while (1)
 	{
 		prompt = readline("lúbia> ");
+		pipe_spaces(prompt);
+		redirections_spaces(prompt);
 		printf("%s\n", prompt);
-		line = ft_split(prompt, ' ');
-		(*ms)->num_cmds = count_commands(line);
-		read_commands(line, cmd, *ms);
+		// line = ft_split(prompt, -1);
+		// (*ms)->num_cmds = count_commands(line);
+		// read_commands(line, cmd, *ms);
 	}
 }
 
