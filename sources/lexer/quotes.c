5df1162b7674be9c00700a74ca69ccebbb333d3b
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-04-28 18:47:56 -0300

    added dollar sign function

--- sources/lexer/quotes.c
+++ sources/lexer/quotes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quotes.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 20:04:09 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/04/26 20:32:01 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/04/28 18:44:53 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,27 @@ void	metachar_inside_quotes(char *prompt)
 					prompt[i] = -4;
 				else if (prompt[i] == '>')
 					prompt[i] = -5;
+				i++;
+			}
+		}
+		i++;
+	}
+}
+
+void	dollar_inside_quotes(char *prompt)
+{
+	int		i;
+
+	i = 0;
+	while (prompt[i])
+	{
+		if (prompt[i] == '\'')
+		{
+			i++;
+			while (prompt[i] != '\'')
+			{
+				if (prompt[i] == '$')
+					prompt[i] = -6;
 				i++;
 			}
 		}
