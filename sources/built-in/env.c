59bf5f9d1879789366820954e7d109d1e893c7b8
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-09 22:22:00 -0300

    created error verification.

--- sources/built-in/env.c
+++ sources/built-in/env.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 21:37:22 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/09 19:37:50 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:12:46 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,6 @@ void	env(t_token *token_list)
 			i++;
 		}
 	}
+	else if (!ft_strncmp(token_list->token[0], "env", 4))
+		printf("env: '%s': No such file or directory\n", token_list->token[1]);
 }
