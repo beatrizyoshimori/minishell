50de9cd4c7d523d9d6ffd0b8bcea56d6b65b1514
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-10 22:29:37 -0300

    changed malloc to calloc and set env_export

--- sources/envp_utils/envp_utils.c
+++ sources/envp_utils/envp_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:12:43 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/05/09 19:13:52 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/10 22:24:01 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,35 @@ void	get_paths(char **envp, t_ms *ms)
 	ms->paths = ft_split(envp[i] + 5, ':');
 }
 
+char	*copy_env_export(char *envp_i)
+{
+	int		j;
+	char	*env_export_i;
+
+	j = ft_strlen(envp_i);
+	if (ft_strchr(envp_i, '='))
+		j += 2;
+	env_export_i = (char *)ft_calloc(j + 1, sizeof(char));
+	j = 0;
+	while (envp_i[j] && envp_i[j] != '=')
+	{
+		env_export_i[j] = envp_i[j];
+		j++;
+	}
+	if (envp_i[j] != '=')
+		return (env_export_i);
+	env_export_i[j] = envp_i[j];
+	env_export_i[j + 1] = '\"';
+	j += 2;
+	while (envp_i[j - 1])
+	{
+		env_export_i[j] = envp_i[j - 1];
+		j++;
+	}
+	env_export_i[j] = '\"';
+	return (env_export_i);
+}
+
 void	copy_envp(char **envp, t_ms *ms)
 {
 	int	i;
@@ -34,12 +63,14 @@ void	copy_envp(char **envp, t_ms *ms)
 	while (envp[i])
 		i++;
 	ms->env_nbr_ptr = i;
-	ms->env = (char **)malloc(sizeof(char *) * (i + 1));
-	ms->env[i] = NULL;
+	ms->env_exp_nbr_ptr = i;
+	ms->env = (char **)ft_calloc(i + 1, sizeof(char *));
+	ms->env_export = (char **)ft_calloc(i + 1, sizeof(char *));
 	i = 0;
 	while (envp[i])
 	{
 		ms->env[i] = ft_strdup(envp[i]);
+		ms->env_export[i] = copy_env_export(envp[i]);
 		i++;
 	}
 }
