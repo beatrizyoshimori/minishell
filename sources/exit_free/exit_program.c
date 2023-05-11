4f3d21d669dd52d818e2a6b78dfa87ea1a5a0a0e
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-10 22:31:10 -0300

    added free ms->env_export

--- sources/exit_free/exit_program.c
+++ sources/exit_free/exit_program.c
@@ -3,64 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   exit_program.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 17:04:10 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/08 22:01:55 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/05/10 22:30:42 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// static void	check_quotes(char *prompt, char **aux)
-// {
-// 	int		i;
-// 	int		length;
-// 	char	c;
-
-// 	i = 0;
-// 	length = ft_strlen(prompt);
-// 	if (prompt[0] == '\'' || prompt[0] == '\"')
-// 		c = prompt[0];
-// 	while (prompt[i] && prompt[i] == c)
-// 		i++;
-// 	length--;
-// 	while (prompt[length] == c)
-// 	{
-// 		length--;
-// 		i--;
-// 	}
-// 	if (i == 0 && prompt[length] == 't')
-// 	{
-// 		free(*aux);
-// 		*aux = ft_strtrim(prompt, &c);
-// 	}
-// 	else if (i == 0 && (prompt[length] == '\'' || prompt[length] == '\"'))
-// }
-
-// void	exit_command(char *prompt, t_ms *ms)
-// {
-// 	char	*aux;
-
-// 	aux = ft_strdup(prompt);
-// 	check_quotes(prompt, &aux);
-// 	if (!ft_strncmp(aux, "exit", 5))
-// 	{
-// 		rl_clear_history();
-// 		free_ptrptr(ms->paths);
-// 		free(ms);
-// 		free(aux);
-// 		free(prompt);
-// 		exit(0);
-// 	}
-// 	else
-// 		free(aux);
-// }
 void	exit_program(t_token **token_list, t_ms *ms)
 {
 	rl_clear_history();
 	free_ptrptr(ms->paths);
 	free_ptrptr(ms->env);
+	free_ptrptr(ms->env_export);
 	free(ms);
 	free_token_list(token_list);
 	exit(1);
