0076584786b04cf1645a2d119c026988e1239ca2
Lucas De Souza Santos <lucade-s@c3r9p5.42sp.org.br>
2023-05-16 20:11:36 -0300

    set g_ms.home

--- sources/envp_utils/envp_utils.c
+++ sources/envp_utils/envp_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:12:43 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/05/14 20:27:36 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:17:29 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,16 @@ void	copy_envp(char **envp)
 		i++;
 	g_ms.env_nbr_ptr = i;
 	g_ms.env = (char **)ft_calloc(i + 1, sizeof(char *));
+	g_ms.home = (char *)ft_calloc(1, sizeof(char));
 	i = 0;
 	while (envp[i])
 	{
 		g_ms.env[i] = ft_strdup(envp[i]);
+		if (!ft_strncmp(envp[i], "HOME=", 5))
+		{
+			free(g_ms.home);
+			g_ms.home = ft_strdup(envp[i] + 5);
+		}
 		i++;
 	}
 }
