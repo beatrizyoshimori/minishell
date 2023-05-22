9c2f2f5fecdd816990a6f7275587126f59daf93c
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-22 17:38:13 -0300

    removed an unecessary tab on a variable declaration

--- libft/sources/ft_lstsize.c
+++ libft/sources/ft_lstsize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byoshimo <byoshimo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 00:38:57 by byoshimo          #+#    #+#             */
-/*   Updated: 2022/09/23 02:46:12 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/05/22 17:36:39 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (lst != NULL)
