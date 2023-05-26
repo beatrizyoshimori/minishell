1f81166f5c763eddd4d94b7ddcee4618bf82e443
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-26 17:20:33 -0300

    added a space on the error message.

--- sources/built-in/pwd.c
+++ sources/built-in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 21:15:08 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/25 18:03:37 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/26 16:47:40 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	pwd(void)
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 	{
-		ft_putstr_fd("bilu: pwd:", 2);
+		ft_putstr_fd("bilu: pwd: ", 2);
 		ft_putstr_fd(pwd, 2);
 		ft_putstr_fd(": ", 2);
 		ft_putstr_fd(strerror(errno), 2);
