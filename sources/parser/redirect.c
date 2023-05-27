3cb3cd9572c876d65c811573b0bb30a1b0af8828
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-05-26 22:48:52 -0300

    fixed redirects.

--- sources/parser/redirect.c
+++ sources/parser/redirect.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:28:40 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/24 19:20:56 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/05/26 22:24:41 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	heredoc(t_token *token, int i)
 	int		fd_heredoc;
 	char	*prompt;
 
-	fd_heredoc = open("heredoc", O_RDWR | O_CREAT | O_TRUNC, 0644);
+	fd_heredoc = open(".h*e*r*e*d*o*c*", O_RDWR | O_CREAT | O_TRUNC, 0644);
 	prompt = NULL;
 	while (1)
 	{
@@ -29,8 +29,7 @@ void	heredoc(t_token *token, int i)
 		free(prompt);
 	}
 	free(prompt);
-	printf("fd heredoc: %i\n", fd_heredoc);
-	// return (fd_heredoc);
+	close(fd_heredoc);
 }
 
 static void	update_token(char **token, char redirect)
@@ -41,7 +40,7 @@ static void	update_token(char **token, char redirect)
 	i = 0;
 	while (token[i])
 	{
-		if (token[i][0] == redirect)
+		if (token[i][0] == redirect && (token[i][1] == redirect || !token[i][1]))
 		{
 			free(token[i]);
 			free(token[i + 1]);
@@ -75,11 +74,35 @@ void	redirect_output(t_token *token_list)
 			if (aux->token[i][0] == '>')
 			{
 				if (!aux->token[i][1])
+				{
 					aux->fd[1] = open(aux->token[i + 1],
 							O_RDWR | O_CREAT | O_TRUNC, 0644);
-				else
+					if (aux->fd[1] == -1)
+					{
+						ft_putstr_fd("bilu: ", 2);
+						ft_putstr_fd(aux->token[i + 1], 2);
+						ft_putstr_fd(": ", 2);
+						ft_putstr_fd(strerror(errno), 2);
+						ft_putstr_fd("\n", 2);
+						g_ms.exit_status = 1;
+						break ;
+					}
+				}
+				else if (aux->token[i][1] == '>')
+				{
 					aux->fd[1] = open(aux->token[i + 1],
 							O_RDWR | O_CREAT | O_APPEND, 0644);
+					if (aux->fd[1] == -1)
+					{
+						ft_putstr_fd("bilu: ", 2);
+						ft_putstr_fd(aux->token[i + 1], 2);
+						ft_putstr_fd(": ", 2);
+						ft_putstr_fd(strerror(errno), 2);
+						ft_putstr_fd("\n", 2);
+						g_ms.exit_status = 1;
+						break ;
+					}
+				}
 				if (aux->redirect == NO_REDIRECT)
 					aux->redirect = REDIRECT_OUTPUT;
 				else if (aux->redirect == REDIRECT_INPUT)
@@ -108,11 +131,21 @@ void	redirect_input(t_token *token_list)
 				if (!aux->token[i][1])
 				{
 					aux->fd[0] = open(aux->token[i + 1], O_RDONLY);
+					if (aux->fd[0] == -1)
+					{
+						ft_putstr_fd("bilu: ", 2);
+						ft_putstr_fd(aux->token[i + 1], 2);
+						ft_putstr_fd(": ", 2);
+						ft_putstr_fd(strerror(errno), 2);
+						ft_putstr_fd("\n", 2);
+						g_ms.exit_status = 1;
+						break ;
+					}
 				}
-				else
+				else if (aux->token[i][1] == '<')
 				{
 					heredoc(aux, i);
-					aux->fd[0] = open("heredoc", O_RDONLY);
+					aux->fd[0] = open(".h*e*r*e*d*o*c*", O_RDONLY);
 				}
 				if (aux->redirect == NO_REDIRECT)
 					aux->redirect = REDIRECT_INPUT;
