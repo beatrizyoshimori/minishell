1d9624f8c5cda3e52cb92f39666526e8447447e7
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-27 20:02:54 -0300

    changed no_exec name to exec.

--- sources/token/token_utils.c
+++ sources/token/token_utils.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 19:39:12 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/27 19:35:28 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/27 19:57:47 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	set_tokens(char **tokens, t_token **token_list)
 		new = (t_token *)malloc(sizeof(t_token));
 		new->token = ft_split(tokens[g_ms.num_tokens], SPACE_OUT_QUOTES);
 		new->redirect = 1;
-		new->no_exec = 0;
+		new->exec = 0;
 		new->pathname = NULL;
 		new->next = NULL;
 		ft_tokenadd_back(token_list, new);
