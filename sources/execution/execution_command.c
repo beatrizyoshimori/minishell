759feb82818ddf7936376c188057857eec4e362f
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-30 22:19:24 -0300

    changed exit_command and print error functions and added free(pid)

--- sources/execution/execution_command.c
+++ sources/execution/execution_command.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execution_cmd_utils.c                              :+:      :+:    :+:   */
+/*   execution_command.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 21:59:18 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/05/29 22:00:05 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/30 19:11:13 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,11 @@ void	exit_execve(t_token *token_list)
 	rl_clear_history();
 	free_token_list(&token_list);
 	free(g_ms.pipe_fd);
+	free(g_ms.pid);
 	free_ptrptr(g_ms.paths);
 	free_ptrptr(g_ms.env);
 	free(g_ms.home);
 	exit(g_ms.exit_status);
-}
-
-static void	make_execve(t_token *token)
-{
-	if (execve(token->pathname, token->token, g_ms.env) == -1)
-	{
-		if (!g_ms.printed_error)
-		{
-			print_error("", token->token[0], strerror(errno));
-			g_ms.exit_status = errno;
-		}
-	}
 }
 
 void	exec_command_child(t_token *token_list, t_token *token)
@@ -46,7 +35,7 @@ void	exec_command_child(t_token *token_list, t_token *token)
 	else if (!ft_strncmp(token->token[0], "env", 4))
 		env(token->token);
 	else if (!ft_strncmp(token->token[0], "exit", 5))
-		exit_command(token_list);
+		exit_command(token_list, token->token);
 	else if (!ft_strncmp(token->token[0], "export", 7))
 		export(token->token);
 	else if (!ft_strncmp(token->token[0], "pwd", 4))
@@ -54,10 +43,10 @@ void	exec_command_child(t_token *token_list, t_token *token)
 	else if (!ft_strncmp(token->token[0], "unset", 6))
 		unset(token->token);
 	else if (token->pathname)
-		make_execve(token);
-	else if (!token->pathname)
 	{
-		print_error("", token->token[0], "command not found");
-		g_ms.exit_status = 127;
+		if (execve(token->pathname, token->token, g_ms.env) == -1)
+			print_error("", token->token[0], strerror(errno), errno);
 	}
+	else if (!token->pathname)
+		print_error("", token->token[0], "command not found", 127);
 }
