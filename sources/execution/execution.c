16581853b895648cf1efff2bc520173f54946aef
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-05-30 22:21:37 -0300

    added new print_error function and fixed exit status on waitpid

--- sources/execution/execution.c
+++ sources/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 19:48:34 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/29 22:04:26 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/05/30 22:20:33 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,23 @@ void	wait_processes(t_token *token_list, int num_proc)
 {
 	int		i;
 	int		status;
+	int		exit_status;
 	t_token	*aux;
 
 	aux = token_list;
 	i = 0;
 	while (i < num_proc)
 	{
-		waitpid(aux->pid, &status, 0);
+		waitpid(g_ms.pid[i], &status, 0);
 		if (WIFEXITED(status))
-			g_ms.exit_status = WEXITSTATUS(status);
+			exit_status = WEXITSTATUS(status);
 		i++;
 		if (i != num_proc)
 			aux = aux->next->next;
+		else
+			g_ms.exit_status = exit_status;
 	}
+	g_ms.on_fork = 0;
 }
 
 void	start_child_process(t_token *token_list, t_token *token, int i)
@@ -42,10 +46,7 @@ void	start_child_process(t_token *token_list, t_token *token, int i)
 			exec_command_child(token_list, token);
 	}
 	else
-	{
-		print_error("bilu: ", token->token[0], "Is a directory");
-		g_ms.exit_status = 126;
-	}
+		print_error("bilu: ", token->token[0], "Is a directory", 126);
 	exit_execve(token_list);
 }
 
@@ -57,13 +58,14 @@ void	start_processes(t_token *token_list)
 
 	create_pipes();
 	num_proc = (g_ms.num_tokens + 1) / 2;
+	g_ms.pid = (pid_t *)ft_calloc(num_proc + 1, sizeof(pid_t));
 	aux = token_list;
 	i = 0;
 	while (i < num_proc)
 	{
 		g_ms.on_fork = 1;
-		aux->pid = fork();
-		if (!aux->pid)
+		g_ms.pid[i] = fork();
+		if (!g_ms.pid[i])
 			start_child_process(token_list, aux, i);
 		i++;
 		if (i != num_proc)
@@ -71,5 +73,8 @@ void	start_processes(t_token *token_list)
 	}
 	close_fd(token_list);
 	wait_processes(token_list, num_proc);
-	g_ms.on_fork = 0;
+	free(g_ms.pipe_fd);
+	g_ms.pipe_fd = NULL;
+	free(g_ms.pid);
+	g_ms.pid = NULL;
 }
