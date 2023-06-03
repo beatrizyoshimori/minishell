7d8cd8e23942c1a856cb296f1cf93c20f4f97e96
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-06-03 20:11:12 -0300

    created signal folder.

---
+++ sources/signal/signal_handlers.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal_handlers.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 19:40:21 by byoshimo          #+#    #+#             */
+/*   Updated: 2023/06/03 19:51:18 by byoshimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	signal_handler_heredoc(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		g_ms.exit_status = 148;
+		close(g_ms.fd_heredoc);
+		exit_process(g_ms.token_list);
+	}
+}
+
+void	signal_handler(int signal)
+{
+	if (signal == SIGINT && !g_ms.on_fork)
+	{
+		ft_putchar_fd('\n', 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+	}
+	else if (signal == SIGINT && g_ms.on_fork == 1)
+		ft_putchar_fd('\n', 1);
+	if (signal == SIGPIPE)
+	{
+		print_error("", "bilu", "Broken pipe", 141);
+		exit_process(g_ms.token_list);
+	}
+}
