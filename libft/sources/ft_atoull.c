9eaff9ea22717329fb7bd23a8e649e5f2aeb42ae
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-06-21 20:07:07 -0300

    fixed exit command exit code.

--- libft/sources/ft_atoull.c
+++ libft/sources/ft_atoull.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 23:22:35 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/05/31 18:30:55 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/06/21 20:05:18 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 unsigned long long	ft_atoull(const char *nptr)
 {
 	int					flag;
-	unsigned long long	num;
+	unsigned long long	nb;
 
 	flag = 0;
 	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
@@ -26,16 +26,17 @@ unsigned long long	ft_atoull(const char *nptr)
 		flag = -1;
 	if (flag)
 		nptr++;
-	num = 0;
+	nb = 0;
 	while (ft_isdigit(*nptr))
 	{
-		num = num * 10 + (*nptr - 48);
-		if ((flag != -1 && num > LL_MAX)
-			|| num > (unsigned long long)LL_MAX + 1)
+		nb = nb * 10 + (*nptr - 48);
+		if ((flag != -1 && nb > LL_MAX) || nb > (unsigned long long)LL_MAX + 1)
 			return ((unsigned long long)LL_MAX + 2);
 		nptr++;
 	}
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
 	if (*nptr && !ft_isdigit(*nptr))
 		return ((unsigned long long)LL_MAX + 2);
-	return (num);
+	return (nb);
 }
